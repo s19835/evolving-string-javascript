effd63e67d737747e9ca7bf5525828e2146f2b42
s19835 <s19835@sci.pdn.ac.lk>
2024-09-24 17:16:04 +0530

    Add: random char genearator

--- Algorithm.js
+++ Algorithm.js
@@ -1,2 +1,9 @@
 // set up require variables
 const target = 'HELLO WORLD';
+
+
+// generate random characters
+function randomChar() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTWXYZ ';
+    return chars[Math.floor(Math.random() * chars.length)];
+}
